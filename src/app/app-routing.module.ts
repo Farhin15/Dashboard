3f644281f4b6078462e9607734a6d9e33f1f833e
Farhin15 <farhinshekh8@gmail.com>
2025-05-23 15:39:17 +0530

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { ReportsComponent } from './features/reports/reports.component';
-import { SettingsComponent } from './features/settings/settings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'reports', component: ReportsComponent },
-  { path: 'settings', component: SettingsComponent }
+  { 
+    path: 'dashboard', 
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+ },
+  { 
+    path: 'reports', 
+    loadComponent: () => import('./features/reports/reports.component').then(m => m.ReportsComponent)
+   },
+  { 
+    path: 'settings', 
+    loadComponent: () => import('./features/settings/settings.component').then(m => m.SettingsComponent)
+   }
 ];
 
 @NgModule({
